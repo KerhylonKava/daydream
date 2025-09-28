0c2b517425de6b0be85e387afd9e86e9a3262891
Luca S <lucaperezsalloum@gmail.com>
2025-09-28 15:15:08 -0700

    Fixed player movement

--- giant.gd
+++ giant.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
+var player = null
+var player_in_area = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("giant_idle")
+	self.visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_in_area:
+		$x.visible = true
+		if Input.is_action_just_pressed("x"):
+			self.visible = false
+	elif !player_in_area:
+		$x.visible = false
+		#$QuestBoards.visible = false
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_in_area = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_in_area = true
+	player = body
+	print("player in area")
--- player.gd
+++ player.gd
@@ -5,26 +5,44 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("player_idle")
+	$BrainSlayerWalk.visible = true
+	$BrainSlayerWalk.flip_h = false
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed * delta
-		$Sprite2D.flip_h = true
+		$BrainSlayerWalk.flip_h = true
+		$BrainSlayerWalk.visible = true
+		$BrainSlayerDiagonalAttack.visible = false
 		$AnimationPlayer.play("player_walk")
+		
 	elif Input.is_action_pressed("right"):
+		$BrainSlayerWalk.visible = true
 		$AnimationPlayer.play("player_walk")
+		$BrainSlayerDiagonalAttack.visible = false
+		$BrainSlayerWalk.visible = true
 		velocity.x = speed * delta
+		$BrainSlayerWalk.flip_h = false
+	
 	
 	elif Input.is_action_pressed("up"):
 		#$SootSpriteImage.flip_h = false
 		velocity.y = -speed * delta
+		$BrainSlayerDiagonalAttack.visible = false
+		$BrainSlayerWalk.visible = true
 	
 	elif Input.is_action_pressed("down"):
 		#$SootSpriteImage.flip_h = false
+		$BrainSlayerDiagonalAttack.visible = false
 		velocity.y = speed * delta
+	elif Input.is_action_pressed("x"):
+		$BrainSlayerDiagonalAttack.visible = true
+		$AnimationPlayer.play("player_attack")
+		$BrainSlayerWalk.visible = false
 	
 	else:
 		if velocity.x > 0:
@@ -36,5 +54,7 @@ func _process(delta: float) -> void:
 		if velocity.y < 0:
 			velocity.y -= 0.15 * velocity.y * delta / 0.0166
 		if velocity.x == 0:
+			velocity.y -= 0.15 * velocity.y * delta / 0.0166
+		if velocity.x and velocity.y == 0:
 			$AnimationPlayer.play("player_idle")
 	move_and_slide()
