d4f61035b1a57528d05148651c10fa7cc1a5f738
Kava <154646695+KerhylonKava@users.noreply.github.com>
2025-09-28 13:17:55 -0700

    fixed movement

--- character.gd
+++ character.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var speed := 450/0.0166
+@export var speed := 100/0.0166
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,18 +11,30 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left"):
-		$SootSpriteImage.flip_h = true
+		#$SootSpriteImage.flip_h = true
 		velocity.x = -speed * delta
 	
 	elif Input.is_action_pressed("right"):
-		$SootSpriteImage.flip_h = false
+		#$SootSpriteImage.flip_h = false
 		velocity.x = speed * delta
+	
+	elif Input.is_action_pressed("up"):
+		#$SootSpriteImage.flip_h = false
+		velocity.y = -speed * delta
+	
+	elif Input.is_action_pressed("down"):
+		#$SootSpriteImage.flip_h = false
+		velocity.y = speed * delta
 	
 	else:
 		if velocity.x > 0:
 			velocity.x -= 0.15 * velocity.x * delta / 0.0166
 		if velocity.x < 0:
 			velocity.x -= 0.15 * velocity.x * delta / 0.0166
+		if velocity.y > 0:
+			velocity.y -= 0.15 * velocity.y * delta / 0.0166
+		if velocity.y < 0:
+			velocity.y -= 0.15 * velocity.y * delta / 0.0166
 		if velocity.x == 0:
 			pass ##animation.play("idle")
 	move_and_slide()
